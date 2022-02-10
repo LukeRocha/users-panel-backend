2e230662089b76fe87ab49a3350d0ffdd0a66a74
Lucas <lucas.ar93@gmail.com>
2022-02-10 15:00:19 -0300

    Add a postgres folder to develop postgres based api

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const pool = require("./db");
-
-app.use(express.json());
-
-app.listen(5555, () => {
-  console.log("server is running on port 5555");
-});
--- src/postgres-api/database.sql
+++ src/postgres-api/database.sql
@@ -4,7 +4,7 @@ CREATE DATABASE UOL-userspanel-api
 CREATE TABLE users_data(
   id SERIAL PRIMARY KEY,
   user_name VARCHAR(255),
-  user_mail VARCHAR(255),
+  user_mail VARCHAR(255) UNIQUE,
   user_document NUMBER(11),
   user_phone NUMBER(11),
   user_status VARCHAR(20) 
