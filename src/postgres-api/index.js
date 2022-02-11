b3fe4cc18a87357424e7b5c96648bee93d8db653
Lucas <lucas.ar93@gmail.com>
2022-02-11 15:41:08 -0300

    Add post method with postgres

--- src/postgres-api/index.js
+++ src/postgres-api/index.js
@@ -1,1 +1,34 @@
+const express = require("express");
+const app = express();
+const pool = require("./db");
 
+app.use(express.json());
+
+//Routes
+
+//get all users method
+
+//post a user method
+app.post("/accounts", async (req, res) => {
+  try {
+    const text =
+      "INSERT INTO users_data2 VALUES ($1, $2, $3, $4, $5) RETURNING *";
+    const data = [
+      req.body.name,
+      req.body.mail,
+      req.body.document,
+      req.body.phone,
+      req.body.status,
+    ];
+    const sendNewUser = await pool.query(text, data);
+    res.json(sendNewUser);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//put(edit) a user method
+
+app.listen(5550, () => {
+  console.log("Server is running on port 5550");
+});
